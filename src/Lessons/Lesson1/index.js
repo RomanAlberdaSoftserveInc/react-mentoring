d9219a94cb802e41d32ba08119588a10088f1ca6
Antony <akarac@softserveinc.com>
2021-12-01 14:54:11 +0200

    Lesson1

---
+++ src/Lessons/Lesson1/index.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const element = <div className="test">JSX</div>;
+// console.log(element);
+
+// console.log(React.createElement("div", { className: "test" }, "JSX"));
+
+const BaseComponent = ({ children }) => {
+  //   useEffect(() => {
+  //     const timer = setTimeout(() => {}, 2000);
+
+  //     return () => {
+  //       clearTimeout(timer);
+  //     };
+  //   }, [userId]);
+
+  return (
+    <div>
+      {element}
+      {children}
+    </div>
+  );
+};
+
+class ClassComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      a: 2,
+    };
+
+    this.ref = React.createRef();
+  }
+
+  componentDidMount() {
+    this.load?.();
+  }
+  componentDidUpdate() {
+    //   prevprops !== this.props && this.load?.();
+  }
+  componentWillUnmount() {}
+
+  render() {
+    return "ClassComponent";
+  }
+}
+
+const Lesson1 = () => {
+  const classComponentRef = useRef(null);
+  //   const component = (
+  //     <BaseComponent ref={classComponentRef} prop1="t1">
+  //       element2
+  //     </BaseComponent>
+  //   );
+  //   console.log(component);
+
+  const classComponent = <ClassComponent ref={classComponentRef} prop1="p1" />;
+  console.log(classComponent);
+  console.log(classComponentRef.current);
+  const a = {};
+
+  console.log(a?.b?.c?.d ?? "111");
+
+  return classComponent;
+  //   return component;
+};
+
+export default Lesson1;
