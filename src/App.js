ddc0a69f575a91570d91761aa84effd9f574f71d
Antony <akarac@softserveinc.com>
2021-12-15 14:55:00 +0200

    Lesson2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css";
-
 import Lesson1 from "./Lessons/Lesson1";
+import Lesson2 from "./Lessons/Lesson2";
 
 function App() {
   return (
-    <div className="App">
-      <Lesson1 />
+    <div className="App" style={{ padding: "1rem" }}>
+      {/* <Lesson1 /> */}
+      <Lesson2 />
     </div>
   );
 }
