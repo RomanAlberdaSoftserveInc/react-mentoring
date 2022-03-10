e6acd780c353bf3a4989911f5917491f97b089c5
Antony <akarac@softserveinc.com>
2022-03-10 14:31:58 +0200

    Lesson9

--- src/Lessons/Lesson8/index.js
+++ src/Lessons/Lesson8/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const MoreText = ({ children }) => {
   useEffect(() => {
@@ -28,21 +28,65 @@ const MaybeError = ({ text, moreText, error }) => {
   );
 };
 
+// class Timer extends React.Component {
+//   state = {
+//     time: 0,
+//   };
+//   constructor() {
+//     super();
+//     this.timer = null;
+//   }
+
+//   componentDidMount() {
+//     this.timer = setInterval(() => {
+//       console.log("HERE");
+//       this.setState(({ time }) => ({ time: time + 1 }));
+//     }, 1000);
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.timer);
+//   }
+
+//   render() {
+//     return `Elapsed time: ${this.state.time}s`;
+//   }
+// }
+
+const Timer = ({ children, limit }) => {
+  const [time, setTime] = useState(0);
+  // const timer = useRef(null);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log("HERE");
+      setTime(time => time + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return time > limit ? "Sorry" : children;
+};
+
 const Lesson8 = () => {
   const [error, setError] = useState("");
   const isLongError = error.length > 10;
 
   return (
     <>
+      <Timer limit={10}>Content</Timer>
       {/* <MaybeError text="text" moreText="More TEXT" error={error} /> */}
-      <br />
+      {/* <br />
       <br />
       <input
         key={isLongError}
         value={error}
         onChange={e => setError(e.target.value)}
       />
-      {isLongError && "TOO MUCH"}
+      {isLongError && "TOO MUCH"} */}
     </>
   );
 };
